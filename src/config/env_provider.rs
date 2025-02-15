43c376789a2a95a4d0362a35339e8011b1a7bb5f
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-02-15 15:19:23 +0530

    env config new function

--- src/config/env_provider.rs
+++ src/config/env_provider.rs
@@ -4,14 +4,22 @@ use anyhow::{Context, Result};
 
 use super::interface::ConfigProvider;
 
-pub struct EnvConfigProvider;
+pub struct EnvConfigProvider {
+    port: u32,
+}
 
-impl ConfigProvider for EnvConfigProvider {
-    fn get_port(&self) -> Result<u32> {
+impl EnvConfigProvider {
+    pub fn new() -> Result<Self> {
         let port_str = env::var("PORT").context("'PORT' environment variable is not set")?;
         let port = port_str
             .parse()
             .context("Failed to parse 'PORT' as a valid u32")?;
-        Ok(port)
+        Ok(Self { port })
+    }
+}
+
+impl ConfigProvider for EnvConfigProvider {
+    fn get_port(&self) -> Result<u32> {
+        Ok(self.port)
     }
 }
