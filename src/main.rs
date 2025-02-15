fbfd4cd3a528eb4b116cb1c7d1680c4092762422
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-02-15 15:31:13 +0530

    server setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use anyhow::Context;
+use axum::Router;
+use quill_quest::config::{env_provider::EnvConfigProvider, interface::ConfigProvider};
+use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    return Ok(());
+    let config_provider = EnvConfigProvider::new().context("Failed to create EnvConfigProvider")?;
+
+    let port = config_provider
+        .get_port()
+        .context("Failed to retrieve port from configuration")?;
+
+    let listener = TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .context(format!("Failed to bind to port {port}"))?;
+
+    let router = Router::new();
+
+    axum::serve(listener, router)
+        .await
+        .context("Server encountered an error")
 }
